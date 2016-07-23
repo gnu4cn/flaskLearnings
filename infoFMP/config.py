e71badd76c00ccfbd556c90972c372ba8821cb30
Peng Hailin <unisko@gmail.com>
2016-07-23 10:39:35 +0800

    20160723, rm infoFMP

--- infoFMP/config.py
+++
@@ -1,8 +0,0 @@
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://unisko:091517@localhost/demo'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    UPLOAD_FOLDER_IMG = 'images'
-    SQLALCHEMY_ECHO = False
-    SQLALCHEMY_POOL_SIZE = 10
-    DEBUG = True
-    SECRET_KEY = '\n\xd79\xf70\n\x1f\x80+Bv\xeb:\xd4k\x87\xe3\x97\x8f\x91\xff;\xda\x1a'
--- infoFMP/www-root/bower_components/angular-bootstrap-lightbox/index.js
+++
@@ -1,2 +0,0 @@
-require('./dist/angular-bootstrap-lightbox');
-module.exports = 'angular-bootstrap-lightbox';
